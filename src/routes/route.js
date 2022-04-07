93dbc63d4d2d682a5c84500600626da9cb975dd5
miryala veronica <101985218+veronica17-web@users.noreply.github.com>
2022-04-08 01:34:45 +0530

    my api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,17 +2,72 @@ const express = require('express');
 const logger = require('./logger')
 
 const router = express.Router();
+//problem 1
+router.get('/movies', function (req,res){
+    const arr = ["rrr", "pushps", "radheshyam", "venom", "wonder women"]
+    res.send(arr)
+})
+//problem 2 and 3
+router.get( '/movies/:indexnumber', function( req,res){
+    const arr = ["rrr", "pushps", "radheshyam", "venom", "wonder women"]
+    const id = req.params.indexnumber
+    if ( id <= arr.length){
+        res.send( arr[id])
+    } else{
+        res.send("invalid")
+    }
+})
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
-});
+//problem 4
+router.get( '/films',function( req,res){
+    const example = [ {
+        "id": 1,
+        "name": "rrr"
+       }, {
+        "id": 2,
+        "name": "pushpa"
+       }, {
+        "id": 3,
+        "name": "radheshayam"
+       }, {
+        "id": 4,
+        "name": "venom"
+    }, {
+        "id": 5,
+        "name": "wonder women"
+       }]
+           
+        res.send(example)
 
+})
 
-
+//problem 5
+router.get( '/films/:filmid', function( req,res){
+    const example = [ {
+        "id": 1,
+        "name": "rrr"
+       }, {
+        "id": 2,
+        "name": "pushpa"
+       }, {
+        "id": 3,
+        "name": "radheshayam"
+       }, {
+        "id": 4,
+        "name": "venom"
+    }, {
+        "id": 5,
+        "name": "wonder women"
+       }]
+           const id = req.params.filmid ;
+           if ( id<= example.length){
+           res.send( example[id])
+           }else {
+               res.send("not found")
+           }
+})        
 
 module.exports = router;
 // adding this comment for no reason
+
+ 
