b277702994a5eb829dc151810d43cb8d6e911f51
miryala veronica <101985218+veronica17-web@users.noreply.github.com>
2022-04-09 16:25:11 +0530

    mypostapi

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,52 @@
 const express = require('express');
+const { route } = require('express/lib/application');
 const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
-});
-
-
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+ 
+       //router.post('/players',function (req, res) {
+        router.post('/players',function (req, res){
+         let insert = req.body;
+    for ( let i=0;i<players.length;i++){
+    if( players[i].name==insert.name)
+     return res.send(  {mes : "already existing"})
+     }
+   players.push( insert);
+   res.send( players)
+     })
 
 
 module.exports = router;
 // adding this comment for no reason
+
